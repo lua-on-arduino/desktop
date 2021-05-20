e4ffa121fae908397c7a15501b6787620f21aa7e
arnoson <omasocke@hotmail.de>
2021-05-20 15:57:33 +0200

    Fix slip implementation

--- src/utils/slip.js
+++ src/utils/slip.js
@@ -1,9 +1,18 @@
 // @ts-check
 
-import { decodePacket } from 'protocol-slip'
-export { encodeMessage as slipEncode } from 'protocol-slip'
+import { decodePacket, encodeMessage } from 'protocol-slip'
 
 /** @param {Buffer} buffer */
 // @ts-ignore (`decodePacket` expects a BufferStreamList which doesn't seem to
 // work. Just passing a Buffer works fine though)
 export const slipDecode = buffer => decodePacket(buffer)
+
+/**
+ * @param {Buffer} buffer
+ * @returns {Buffer}
+ */
+export const slipEncode = buffer =>
+  // `encodeMessage()` omits the beginning `0xc0` sign, but the arduino slip
+  // implementation needs it. Both standards seem to be valid:
+  // (https://de.wikipedia.org/wiki/Serial_Line_Internet_Protocol)
+  Buffer.concat([Buffer.from([0xc0]), encodeMessage(buffer)])
