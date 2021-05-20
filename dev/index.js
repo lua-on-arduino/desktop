542cff53ed11d2e930a415fa3427a5487c376078
arnoson <omasocke@hotmail.de>
2021-05-20 18:10:08 +0200

    Cleanup

--- dev/index.js
+++ dev/index.js
@@ -1,13 +1,9 @@
 import { LuaOnArduino } from '../src/index.js'
-import { delay } from '../src/utils/delay.js'
 
 const loa = new LuaOnArduino()
 
 async function main() {
   await loa.connect()
-  await delay(100)
-
-  // console.log((await loa.listDirectory('lua'))?.toString())
 
   loa.syncDirectory('./lua/**/*.*', { watch: true })
 }
