ac4c857f48e37890616163d1a60967a5033f90b2
arnoson <omasocke@hotmail.de>
2021-10-05 14:47:06 +0200

    Cleanup

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
 export * from './dirIncludes'
 export * from './getMessageId'
 export * from './highlightLuaDump'
-export * from './parsePathPattern'
 export * from './pathToPosix'
 export * from './startsWith'
